a6d49275649e457269910d488f97307c73de73dc
dell <dell@DESKTOP-4TMG2GA>
2024-02-03 15:40:33 -0500

    Add project files.

---
+++ practice test/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// main menu
+Console.WriteLine("\nmain menu");
+Console.WriteLine("1.feed");
+Console.WriteLine("2.play with pet");
+Console.WriteLine("3.let rest");
+Console.WriteLine("4.check");
+Console.WriteLine("5.exit");
+
+Console.Write("user input: ");
+int petcare = Convert.ToInt32(Console.ReadLine());
+    }
